359cdfbd02cdb54d6e87e0429a9461825374465b
ibnatysalsabila <salsajpr01@gmail.com>
2025-10-22 21:19:02 +0700

    Tugas 3

---
+++ Tugas3/Tugas3.md
@@ -0,0 +1,16 @@
+# Tugas 3
+
+1. Tampilan Endpoint Presensi Check-in (POST)
+![Tampilan Endpoint Create](SS/Check-in.png)
+
+2. Tampilan Endpoint Presensi Check-in lebih dari sekali (POST)
+![Tampilan Endpoint Create](SS/Check-in(2).png)
+
+3. Tampilan Endpoint  Presensi Check-out (POST)
+![Tampilan Endpoint Create](SS/Check-out.png)
+
+4. Tampilan Endpoint  Presensi Check-out belum melakukan check-in (POST)
+![Tampilan Endpoint Create](SS/Check-out(2).png)
+
+5. Tampilan Endpoint reportsdaily (GET)
+![Tampilan Endpoint Create](SS/Reportsdaily.png)
--- my-node-server/server.js
+++ my-node-server/server.js
@@ -1,35 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
-const bookRoutes = require('./routes/books');
+const morgan = require("morgan");
 
-// 🟢 Middleware utama
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-
-// 🟢 Middleware logging
+app.use(morgan("dev"));
 app.use((req, res, next) => {
-  const timestamp = new Date().toISOString();
-  console.log('[${timestamp}] ${req.method} ${req.url}');
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
-
-// 🟢 Routing
-app.use('/api/books', bookRoutes);
-
-// 🟠 404 Handler (Not Found)
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route not found' });
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
 });
-
-// 🔴 Global Error Handler
-app.use((err, req, res, next) => {
-  console.error('Error:', err.stack);
-  res.status(500).json({ message: 'Internal Server Error' });
-});
-
-// 🟢 Jalankan server
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
 app.listen(PORT, () => {
-  console.log('Server running at http://localhost:${PORT}/');
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
